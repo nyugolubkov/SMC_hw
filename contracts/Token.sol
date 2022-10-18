3b9d27039fa27753dde375d66255072b3a9df456
Nikita Golubkov <nyugolubkov@edu.hse.ru>
2022-10-18 16:53:58 +0300

    web3 counter

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,4 +9,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
 
     constructor(string memory tokenName, string memory tokenSymbol) ERC20(tokenName, tokenSymbol) {}
+
+    event ValueChanged(uint oldValue, uint256 newValue);
+
+    uint256 private count = 0;
+
+    function increment() public {
+        count += 1;
+        emit ValueChanged(count - 1, count);
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
 }
